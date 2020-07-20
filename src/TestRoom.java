305b4f73a4ed678b56abd1c2311b644b2d135f46
Nathan Getachew <nathygech111@gmail.com>
2020-07-20 11:44:50 +0300

    Count Bits

--- src/TestRoom.java
+++ src/TestRoom.java
@@ -1,9 +1,12 @@
 
+import primitive_types.BitTrial;
+
 import java.util.*;
 
 public class TestRoom {
 
     public static void main(String[] args) {
-
+        BitTrial bitTrial = new BitTrial();
+        bitTrial.countBits(6);
     }
 }
---
+++ src/primitive_types/BitTrial.java
@@ -0,0 +1,16 @@
+package primitive_types;
+
+public class BitTrial {
+
+    public int countBits(int x) {
+        short numBits = 0;
+        while (x != 0) {
+            int s = x & 1;
+            System.out.println(s);
+            numBits += (x & 1);
+            x >>>= 1;
+        }
+        return numBits;
+    }
+
+}
