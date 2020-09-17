a54f7aedec715e73bf6b6f3494341041eb660cd0
Nathan Getachew <nathygech111@gmail.com>
2020-09-17 23:49:15 +0300

    Sliding window average

--- src/array/SLN1.java
+++ src/array/SLN1.java
@@ -1,7 +1,24 @@
 package array;
+import java.util.Arrays;
 
 public class SLN1 {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(slidingWindowAvg(new int[] {1, 3, 2, 6, -1, 4, 1, 8, 2}, 5)));
     }
-}
+
+    public static double[] slidingWindowAvg(int[] arr, int k) {
+        double[] result = new double[arr.length - k + 1]; // O(1)
+        int start = 0, end = k - 1, winSum = 0;           // O(1)
+        while (end < arr.length) {                        // O(N - K - 1)
+            for (int i = start; i < end; i++) {           // O(K)
+                winSum += arr[i];                         // O(1)
+            }                                             // O(K)
+            result[start] = (double) winSum / k;          // O(1)
+            winSum = 0;                                   // O(1)
+            ++end;                                        // O(1)
+            ++start;                                      // O(1)
+        }
+        return result;
+    }
+    // O(N - k) * K
+ }
