7a08e5c46b73ae5ff0761eefce30409cdfa190a1
nathan-g1 <nathygech111@gmail.com>
2024-06-26 15:33:47 -0400

    Product of array elements except self
    - TODO: Many variations

---
+++ src/array/ArrayProduct.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+class ArrayProduct {
+    public static void main(String[] args) {
+        // NOTE: You can use the following input values to test this function.
+        int[] a1 = {-1, 3, 8, 2, 9, 5};
+//        var val = productExceptSelf(a1);
+//        System.out.println(Arrays.toString(val));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        var size = nums.length;
+        int[] prefix = new int[size];
+        int[] sufix = new int[size];
+        for (int i = 1; i < size; i++) {
+            prefix[i] *= prefix[i - 1];
+        }
+
+        System.out.println(Arrays.toString(prefix));
+//        for (int i =)
+        return new int[] {};
+    }
+}
