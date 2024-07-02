3c6727fd297cb433b8fe8f931787c4c4146157dd
nathan-g1 <nathygech111@gmail.com>
2024-07-02 12:33:07 -0400

    Fix java linting issues

--- src/array/ArrayProduct.java
+++ src/array/ArrayProduct.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 class ArrayProduct {
     public static void main(String[] args) {
         // NOTE: You can use the following input values to test this function.
-        int[] a1 = {-1, 3, 8, 2, 9, 5};
+        // int[] a1 = {-1, 3, 8, 2, 9, 5};
 //        var val = productExceptSelf(a1);
 //        System.out.println(Arrays.toString(val));
     }
 
-    public static int[] productExceptSelf(int[] nums) {
-        var size = nums.length;
+    public static int[] productExceptSelf(int[] arr) {
+        var size = arr.length;
         int[] prefix = new int[size];
-        int[] sufix = new int[size];
+        // int[] suffix = new int[size];
         for (int i = 1; i < size; i++) {
             prefix[i] *= prefix[i - 1];
         }
