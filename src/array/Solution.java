91bf50ebadc5f8530004e47e61b3c5f3f6c52a4c
nathan-g1 <nathygech111@gmail.com>
2025-02-15 13:13:28 -0600

    Grind75: Merge Two Sorted Linked Lists

--- src/array/Solution.java
+++ src/array/Solution.java
@@ -13,6 +13,14 @@ import java.util.Map;
 import java.util.Stack;
 
 public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[] {2,7,11,15}, 9)));
         isValid("()[]{}");
@@ -90,5 +98,46 @@ public class Solution {
             }
         }
         return stack.isEmpty();
+    }
+
+    // #21: Merge Two Sorted Lists
+    /*
+     * You are given the heads of two sorted linked lists list1 and list2.
+
+        Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+        Return the head of the merged linked list.
+
+        Input: list1 = [1,2,4], list2 = [1,3,4]
+        Output: [1,1,2,3,4,4]
+        Example 2:
+
+        Input: list1 = [], list2 = []
+        Output: []
+        Example 3:
+
+        Input: list1 = [], list2 = [0]
+        Output: [0]
+     */
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(0);
+        ListNode head = res;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                head.next = list1;
+                list1 = list1.next;
+            } else {
+                head.next = list2;
+                list2 = list2.next;
+            }
+            head = head.next;
+        }
+
+        if (list1 == null) {
+            head.next = list2;
+        } else if (list2 == null) {
+            head.next = list1;
+        }
+        return res.next;
     }
 }
