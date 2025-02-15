d0a339acccaae4b13fe861125fe477eb8fc90457
nathan-g1 <nathygech111@gmail.com>
2025-02-15 14:20:27 -0600

    Grind75: Best Time to Buy and Sell Stock
    - Non Grind75 is deleting duplicates from linked list

--- src/array/Solution.java
+++ src/array/Solution.java
@@ -140,4 +140,61 @@ public class Solution {
         }
         return res.next;
     }
+
+    // #83: Remove duplicates from Sorted List
+    /*
+     * Given the head of a sorted linked list, 
+     * delete all duplicates such that each element 
+     * appears only once. Return the linked list sorted as well.
+     * 
+     * Input: head = [1,1,2]
+     * Output: [1,2]
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head.next == null || head == null) return head;
+        ListNode newHead = head;
+        ListNode res = newHead;
+        while (res.next != null) {
+            if (res.val == res.next.val) {
+                ListNode temp = res.next.next;
+                res.next = temp;
+            }
+            res = res.next;
+        }
+        return newHead;
+    }
+
+    // #121: Best Time to Buy and Sell Stock
+    /*
+     * 
+     * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+        You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+        Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+        Example 1:
+
+        Input: prices = [7,1,5,3,6,4]
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+        Example 2:
+
+        Input: prices = [7,6,4,3,1]
+        Output: 0
+        Explanation: In this case, no transactions are done and the max profit = 0.
+     */
+    static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
 }
