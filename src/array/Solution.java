9e08a1938a8998449ece0f52ff95025333208997
nathan-g1 <nathygech111@gmail.com>
2025-02-17 16:20:55 -0600

    Add TreeNode class

--- src/array/Solution.java
+++ src/array/Solution.java
@@ -14,6 +14,19 @@ import java.util.Stack;
 
 public class Solution {
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public class ListNode {
         int val;
         ListNode next;
