8a749f6370ef9510e90e85986788a8db8cd4c073
nathan-g1 <nathygech111@gmail.com>
2025-02-17 16:47:19 -0600

    Grind75: Valid Anagrams

--- src/array/Solution.java
+++ src/array/Solution.java
@@ -282,4 +282,36 @@ public class Solution {
         invertTree(root.left);
         invertTree(root.right);
     }
+
+    // 242: Valid Anagrams
+    /*
+     * Given two strings s and t, return true if t is an anagram
+        of s, and false otherwise.
+        Example 1:
+        Input: s = "anagram", t = "nagaram"
+        Output: true
+
+        Example 2:
+        Input: s = "rat", t = "car"
+        Output: false
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] alpCounter = new int[26];
+        for (char c : s.toCharArray()) {
+            int val = (int) c;
+            val = val - 97;
+            alpCounter[val]++;
+        }
+
+        for (char c : t.toCharArray()) {
+            int val = (int) c;
+            val = val - 97;
+            alpCounter[val]--;
+        }
+        for (int i : alpCounter) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
 }
